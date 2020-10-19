07bc01a02c6592f4dfba32f26581ae1b9456de70
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2020-10-19 20:50:10 +0200

    simple app bar

--- app/src/main/java/com/example/mqttplay/AddBrokerActivity.kt
+++ app/src/main/java/com/example/mqttplay/AddBrokerActivity.kt
@@ -1,11 +1,27 @@
 package com.example.mqttplay
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.appbar.MaterialToolbar
 
 class AddBrokerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_broker)
+
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        val toolbar = findViewById<MaterialToolbar>(R.id.addBrokerToolbar)
+
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationOnClickListener {
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        supportActionBar?.title = "Add broker"
     }
 }
--- app/src/main/java/com/example/mqttplay/MainActivity.kt
+++ app/src/main/java/com/example/mqttplay/MainActivity.kt
@@ -1,21 +1,35 @@
 package com.example.mqttplay
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.util.AttributeSet
+import android.view.View
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        return super.onCreateView(name, context, attrs)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var addBrokerBtn = findViewById<FloatingActionButton>( R.id.addBrokerBtn)
+        setupToolbar()
+
+        val addBrokerBtn = findViewById<FloatingActionButton>( R.id.addBrokerBtn)
         addBrokerBtn.setOnClickListener {
             intent = Intent(this, AddBrokerActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun setupToolbar() {
+        val toolbar = findViewById<MaterialToolbar>(R.id.mainActivityToolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Brokers"
     }
 }
