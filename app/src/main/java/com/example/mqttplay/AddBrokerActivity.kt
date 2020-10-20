86d92b06c943132ea4137972e97711247a48be7c
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2020-10-20 12:01:42 +0200

    add broker form simple UI

--- app/src/main/java/com/example/mqttplay/AddBrokerActivity.kt
+++ app/src/main/java/com/example/mqttplay/AddBrokerActivity.kt
@@ -3,6 +3,8 @@ package com.example.mqttplay
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.google.android.material.appbar.MaterialToolbar
 
@@ -12,6 +14,14 @@ class AddBrokerActivity : AppCompatActivity() {
         setContentView(R.layout.activity_add_broker)
 
         setupToolbar()
+        setupSaveBtn()
+    }
+
+    private fun setupSaveBtn() {
+        val saveBtn = findViewById<Button>(R.id.saveBrokerBtn)
+        saveBtn.setOnClickListener {
+            Toast.makeText(this,"Submit", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun setupToolbar() {
@@ -22,6 +32,6 @@ class AddBrokerActivity : AppCompatActivity() {
             intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-        supportActionBar?.title = "Add broker"
+        supportActionBar?.title = "Add broker"  
     }
 }
