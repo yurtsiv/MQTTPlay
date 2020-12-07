a336ca20085bf5637c6739155bd0b7fe4fb41b25
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2020-12-07 21:48:54 +0100

    placeholders for address & port fields

---
+++ app/src/main/java/com/example/mqttplay/view/TextInputWithPlaceholder.kt
@@ -0,0 +1,24 @@
+package com.example.mqttplay.view
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.textfield.TextInputEditText
+
+class TextInputWithPlaceholder : TextInputEditText {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    private val placeholder = hint
+
+    init {
+        hint = ""
+        onFocusChangeListener = OnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                postDelayed({ hint = placeholder }, 100)
+            } else {
+                hint = ""
+            }
+        }
+    }
+}
