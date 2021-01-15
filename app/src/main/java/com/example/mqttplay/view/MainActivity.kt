3b3b3fd2bf15526b5be20c2bd2c20bd9c340236a
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2021-01-15 20:04:32 +0000

    app icon, splash screen & theme improvements

--- app/src/main/java/com/example/mqttplay/view/MainActivity.kt
+++ app/src/main/java/com/example/mqttplay/view/MainActivity.kt
@@ -14,6 +14,9 @@ class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setTheme(R.style.Theme_MQTTPlay)
+
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide()
